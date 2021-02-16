3304c6558533aeb02528fc3a2f502cc32d1f7672
Kyle Pedersen <kylepedersen@berkeley.edu>
2021-02-16 01:06:11 -0800

    init

---
+++ cmake-build-debug/CMakeFiles/cpplib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpplib.dir/main.cpp.o"
+  "cpplib"
+  "cpplib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpplib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
